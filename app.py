93b186e20543f0caaa6d4fabc8ec3bd3fc35900f
Rihanmulani12 <rihanmulani18@gmail.com>
2024-06-13 11:21:16 +0530

    fiexed

--- app.py
+++ app.py
@@ -13,7 +13,7 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-#genai.configure(api_key="AIzaSyAOrOdIzJ_4tEb40oU6v3aAWzITVdjBIlY")
+
 os.getenv("GOOGLE_API_KEY")
 genai.configure(api_key=os.getenv("GOOGLE_API_KEY"))
 
